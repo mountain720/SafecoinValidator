c3fd598f7398e7e1cadd25445fdd76a66b5a40e4
Mountain720 <70184721+mountain720@users.noreply.github.com>
2021-02-25 15:07:08 -0700

    Update safecoin.sh

--- safecoin.sh
+++ safecoin.sh
@@ -10,15 +10,3 @@ cargo build
 sh -c "$(curl -sSfL https://release.solana.com/v1.5.10/install)"
 export PATH="/home/ubuntu/.local/share/solana/install/active_release/bin:$PATH"
 
-safecoin config set --url https://api.mainnet.safecoin.org
-sudo solana-sys-tuner --user $(whoami) > sys-tuner.log 2>&1 &
-sudo sysctl -w vm.max_map_count=500000
-
-#from here, multiple keypairs are created.  you'll need to do this manually for a real validator and make note of your key seed.  for a testnet validator, you don't have to
-
-safecoin-keygen new -o ~/validator-keypair.json
-safecoin config set --keypair ~/validator-keypair.json
-safecoin-keygen new -o ~/vote-account-keypair.json
-safecoin create-vote-account ~/vote-account-keypair.json ~/validator-keypair.json
-safecoin airdrop 10
-
